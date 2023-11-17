dd11166fc692bd923a5573b62a8ca6b21a19af7c
José Pereira <josejunior2002@gmail.com>
2023-11-17 12:20:33 +0000

    Configuração VM

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,18 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+import { io } from 'socket.io-client'
 import App from './App.vue'
+const apiDomain = import.meta.env.VITE_API_DOMAIN
+const wsConnection = import.meta.env.VITE_WS_CONNECTION
+
+const app = createApp(App)
+
+
+app.provide('serverUrl',`${apiDomain}/api`)
+app.provide('socket',io(wsConnection))
+
+
 
 createApp(App).mount('#app')
