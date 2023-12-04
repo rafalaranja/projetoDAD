1183e3b0825ede9026e5529e1c27ffc71a77f88c
Rafael Moreira Nunes <rafaelmoreira2618@gmail.com>
2023-12-04 22:05:30 +0000

    Update UserPolicy.php

--- backend/app/Policies/UserPolicy.php
+++ backend/app/Policies/UserPolicy.php
@@ -5,12 +5,12 @@ class UserPolicy
 {
  public function view(User $user, User $model)
  {
-   return $user->type == "A" || $user->id == $model->id;
+   return $user->user_type == "A" || $user->id == $model->id;
    
  }
  public function update(User $user, User $model)
  {
- return $user->type == "A" || $user->id == $model->id;
+ return $user->user_type == "A" || $user->id == $model->id;
  }
  public function updatePassword(User $user, User $model)
  {
