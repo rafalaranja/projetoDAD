6885bd44fe0eaa381e21b3afade251ce76ea3ddc
alexmiguel23 <2211044@my.ipleiria.pt>
2023-12-11 23:07:46 +0000

    frontend balance

--- frontend/src/components/vcards/VcardDetail.vue
+++ frontend/src/components/vcards/VcardDetail.vue
@@ -1,181 +1,36 @@
 <script setup>
-  import { ref, watch, computed } from 'vue'
+import { ref, watch, computed } from 'vue'
 
-  const props = defineProps({
-    vcard: {
-      type: Object,
-      required: true
-    },
-    operationType: {
-      type: String,
-      default: 'insert'  // insert / update
-    },
-    transactions: {
-      type: Array,
-      required: true
-    },
-    fixedTransaction: {
-      type: Number,
-      default: null
-    },
-    errors: {
-      type: Object,
-      required: false,
-    },
-  })
-
-  const emit = defineEmits(['save', 'cancel'])
-
-  const editingVcard = ref(props.vcard)
-
-  watch(
-    () => props.vcard,
-    (newVcard) => {
-      editingVcard.value = newVcard
-    }
-  )
-
-  watch(
-    () => props.fixedTransaction, 
-    (newFixedTransaction) => {
-      if (newFixedTransaction) {
-        editingVcard.value.transaction_id = newFixedTransaction
-      }
-    },
-    { immediate: true }
-  )
-
-  const vcardTitle = computed( () => {
-    if (!editingVcard.value) {
-        return ''
-      }
-      return props.operationType == 'insert' ? 'New Vcard' : 'Vcard #' + editingVcard.value.id
-  })
-
-  const save = () => {
-    emit('save', editingVcard.value)
+const props = defineProps({
+  vcard: {
+    type: Object,
+    required: true
   }
+})
 
-  const cancel = () => {
-    emit('cancel', editingVcard.value)
-  }
 </script>
 
 <template>
-  <form
-    class="row g-3 needs-validation"
-    novalidate
-    @submit.prevent="save"
-  >
-    <h3 class="mt-5 mb-3">{{ vcardTitle }}</h3>
-    <hr>
-
-    <div class="d-flex flex-wrap justify-content-between">
-      <div class="mb-3 checkCompleted">
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            :class="{ 'is-invalid': errors ? errors['completed'] : false }"
-            type="checkbox"
-            v-model="editingVcard.completed"
-            id="inputCompleted"
-          >
-          <label
-            class="form-check-label"
-            for="inputCompleted"
-          >
-            Vcard is Completed
-          </label>
-          <field-error-message :errors="errors" fieldName="completed"></field-error-message>
-        </div>
-      </div>
-      <div
-        class="row mb-3 total_hours"
-        v-show="editingVcard.completed"
-      >
-        <label
-          for="inputHours"
-          class="col-sm-2 col-form-label"
-        >Hours</label>
-        <div class="col-sm-10">
-          <input
-            type="number"
-            class="form-control"
-            :class="{ 'is-invalid': errors ? errors['total_hours'] : false }"
-            id="inputHours"
-            placeholder="Total hours to complete the vcard"
-            v-model="editingVcard.total_hours"
-          >
-          <field-error-message :errors="errors" fieldName="total_hours"></field-error-message>
-        </div>
-      </div>
+  <div class="d-flex justify-content-between">
+    <div class="mx-2">
+      <h3 class="mt-4">VCard</h3>
     </div>
-
-    <div class="mb-3">
-      <label
-        for="inputDescription"
-        class="form-label"
-      >Description</label>
-      <input
-        type="text"
-        class="form-control"
-        :class="{'is-invalid': errors ? errors['description']: false}"
-        id="inputDescription"
-        placeholder="Vcard Description"
-        required
-        v-model="editingVcard.description"
-      >
-      <field-error-message :errors="errors" fieldName="description"></field-error-message>
+  </div>
+  <hr>
+  <div class="card text-center m-4">
+    <div class="card-header bg-dark text-white">
+      <h5>933282291</h5>
     </div>
-    <div class="mb-3">
-      <label
-        for="inputTransaction"
-        class="form-label"
-      >Transaction</label>
-      <select
-        class="form-select"
-        :class="{ 'is-invalid': errors ? errors['transaction_id'] : false }"
-        id="inputTransaction"
-        :disabled="fixedTransaction"
-        v-model="editingVcard.transaction_id"
-      >
-        <option :value="null">-- No Transaction --</option>
-        <option
-          v-for="prj in transactions"
-          :key="prj.id"
-          :value="prj.id"
-        >{{prj.name}}</option>
-      </select>
-      <field-error-message :errors="errors" fieldName="transaction_id"></field-error-message>
+    <div class="card-body">
+      <h5 class="card-title">{{}}</h5>
+      <h3 class="pt-2">BALANCE</h3>
+      <h5 class="card-text pb-3 pt-1">20€</h5>
     </div>
-    <div class="mb-3">
-      <label
-        for="inputNotes"
-        class="form-label"
-      >Notes</label>
-      <textarea
-        class="form-control"
-        :class="{ 'is-invalid': errors ? errors['notes'] : false }"
-        id="inputNotes"
-        rows="4"
-        v-model="editingVcard.notes"
-      ></textarea>
-      <field-error-message :errors="errors" fieldName="notes"></field-error-message>
+    <div class="card-footer text-muted">
+      <h5>MAX DEBIT</h5>
+      30 000€
     </div>
-
-    <div class="mb-3 d-flex justify-content-end">
-      <button
-        type="button"
-        class="btn btn-primary px-5"
-        @click="save"
-      >Save</button>
-      <button
-        type="button"
-        class="btn btn-light px-5"
-        @click="cancel"
-      >Cancel</button>
-    </div>
-  </form>
+  </div>
 </template>
 
 
@@ -183,6 +38,7 @@
 .total_hours {
   width: 26rem;
 }
+
 .checkCompleted {
   min-height: 2.375rem;
 }
