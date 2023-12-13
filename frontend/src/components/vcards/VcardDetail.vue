c32131c8393575d9a27feb8edf3fe5f2f8750361
José Pereira <josejunior2002@gmail.com>
2023-12-13 10:11:35 +0000

    Update VcardDetail.vue

--- frontend/src/components/vcards/VcardDetail.vue
+++ frontend/src/components/vcards/VcardDetail.vue
@@ -6,12 +6,12 @@ import { useUserStore } from "../../stores/users.js";
 
 const router = useRouter();
 const userStore = useUserStore();
-const tasks = ref(null); // Inicialize tasks como null
+const vcards = ref(null); // Inicialize tasks como null
 
 const loadVCards = async () => {
   try {
     const response = await axios.get("vcard/" + userStore.user.id + "/load");
-    tasks.value = response.data.data; // Atribua response.data.data a tasks.value
+    vcards.value = response.data.data; // Atribua response.data.data a tasks.value
     console.log(response.data.data);
   } catch (error) {
     console.log(error);
@@ -20,24 +20,24 @@ const loadVCards = async () => {
 
 onMounted(loadVCards);
 
-defineExpose({ tasks });
+defineExpose({ vcards });
 </script>
 
 <template>
-  <div class="card text-center m-4" v-if="tasks">
+  <div class="card text-center m-4" v-if="vcards">
     <!-- Verifique se tasks não é null -->
     <div class="card-header bg-dark text-white">
-      <h5>{{ tasks.phone_number }}</h5>
+      <h5>{{ vcards.phone_number }}</h5>
     </div>
     <div class="card-body">
-      <h5 class="card-title">{{ tasks.name }}</h5>
+      <h5 class="card-title">{{ vcards.name }}</h5>
       <!-- Acesse as propriedades de tasks -->
       <h3 class="pt-2">BALANCE</h3>
-      <h5 class="card-text pb-3 pt-1">{{ tasks.balance }}</h5>
+      <h5 class="card-text pb-3 pt-1">{{ vcards.balance }}</h5>
     </div>
     <div class="card-footer text-muted">
       <h5>MAX DEBIT</h5>
-      {{ tasks.max_debit }}€
+      {{ vcards.max_debit }}€
     </div>
   </div>
 </template>
