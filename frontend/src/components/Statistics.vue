6518d1237ae2ca749393607d8e7c037059d82dc5
Rafael Santos <rafaelfsantos03@gmail.com>
2023-12-15 04:28:06 +0000

    Update Statistics.vue

--- frontend/src/components/Statistics.vue
+++ frontend/src/components/Statistics.vue
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const canvasRef = ref(null);    //graph1
 const pieCanvasRef = ref(null); //graph2
-const lineCanvasRef = ref(null); //graph3
+const bubbleCanvasRef = ref(null); //graph3
 const linearCanvasRef = ref(null); //graph4
 const statistics = ref([]);
 
@@ -21,172 +21,174 @@ const loadStatistics = async () => {
 };
 
 onMounted(async () => {
-    const ctx = canvasRef.value.getContext('2d');
-    const pieCtx = pieCanvasRef.value.getContext('2d');
-    const lineCtx = lineCanvasRef.value.getContext('2d');
-    const linearCtx = linearCanvasRef.value.getContext('2d');
+  const ctx = canvasRef.value.getContext('2d');
+  const pieCtx = pieCanvasRef.value.getContext('2d');
+  const bubbleCtx = bubbleCanvasRef.value.getContext('2d');
+  const linearCtx = linearCanvasRef.value.getContext('2d');
 
-    loadStatistics();
+  loadStatistics();
 
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+        'November', 'December'],
+      datasets: [{
+        label: '# of Transactions',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  new Chart(linearCtx, {
+    type: 'line',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+        'November', 'December'],
+      datasets: [{
+        data: [12, 19, 30, 5, 22, 3, 11, 2, 10, 44, 5, 67],
+        backgroundColor: ['#3498db', '#2ecc71', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'],
+      }]
+    },
+    options: {
+      responsive: true,
+    }
+  });
+
+  new Chart(pieCtx, {
+    type: 'pie',
+    data: {
+      labels: ['MB', 'IBAN', 'VCard', 'Visa', 'Paypal', 'MBWAY'],
+      datasets: [{
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ['#3498db', '#2ecc71', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'],
+      }]
+    },
+    options: {
+      responsive: true,
+    }
+  });
+
+  new Chart(bubbleCtx, {
+    type: 'bubble',
+    data: {
+      labels: ['0-2000', '2000-5000', '5000-10000', '10000-50000', '50000-100000', '100000+'],
+      datasets: [{
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ['#3498db', '#2ecc71', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'],
+      }]
+    },
+    options: {
+      responsive: true,
+    }
+  });
+
+  watch(statistics, () => {
     new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
-                'November', 'December'],
-            datasets: [{
-                label: '# of Transactions',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+      type: 'bar',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+          'November', 'December'],
+        datasets: [{
+          label: '# of Transactions',
+          data: statistics.value,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
         }
+      }
     });
-
-    new Chart(linearCtx, {
-        type: 'line',
-        data: {
-            labels: ['MB', 'IBAN', 'VCard', 'Visa', 'Paypal', 'MBWAY'],
-            datasets: [{
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: ['#3498db', '#2ecc71', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'],
-            }]
-        },
-        options: {
-            responsive: true,
-        }
-    });
-
-    new Chart(pieCtx, {
-        type: 'pie',
-        data: {
-            labels: ['MB', 'IBAN', 'VCard', 'Visa', 'Paypal', 'MBWAY'],
-            datasets: [{
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: ['#3498db', '#2ecc71', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'],
-            }]
-        },
-        options: {
-            responsive: true,
-        }
-    });
-
-    new Chart(lineCtx, {
-        type: 'line',
-        data: {
-            labels: ['MB', 'IBAN', 'VCard', 'Visa', 'Paypal', 'MBWAY'],
-            datasets: [{
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: ['#3498db', '#2ecc71', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'],
-            }]
-        },
-        options: {
-            responsive: true,
-        }
-    });
-
-    watch(statistics, () => {
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
-                    'November', 'December'],
-                datasets: [{
-                    label: '# of Transactions',
-                    data: statistics.value,
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    });
+  });
 });
 </script>
 
 <template>
-    <div class="graph-container">
-      <div class="card m-4">
-        <div class="card-header">Number of Transactions per Month</div>
-        <div class="card-body">
-          <canvas ref="linearCanvasRef"></canvas>
-        </div>
+  <div class="graph-container">
+    <div class="card m-4">
+      <div class="card-header">Average Transaction Amount</div>
+      <div class="card-body">
+        <canvas ref="linearCanvasRef"></canvas>
       </div>
-      <div class="card m-4">
-        <div class="card-header">Payment Type Distribution</div>
-        <div class="card-body d-flex justify-content-center">
-          <div class="canvas-pie">
-            <canvas ref="pieCanvasRef"></canvas>
-          </div>
-        </div>
-        </div>
-        <div class="card m-4">
-        <div class="card-header">Payment Type Distribution</div>
-        <div class="card-body d-flex justify-content-center">
-          <div class="canvas-pie">
-            <canvas ref="lineCanvasRef"></canvas>
-          </div>
+    </div>
+    <div class="card m-4">
+      <div class="card-header">Payment Type Distribution</div>
+      <div class="card-body d-flex justify-content-center">
+        <div class="canvas-pie">
+          <canvas ref="pieCanvasRef"></canvas>
         </div>
       </div>
     </div>
     <div class="card m-4">
-        <div class="card-header">Payment Type Distribution</div>
-        <div class="card-body d-flex justify-content-center">
-          <div class="canvas">
-            <canvas ref="canvasRef"></canvas>
-          </div>
+      <div class="card-header">VCard Balance Distribution</div>
+      <div class="card-body d-flex justify-content-center">
+        <div class="canvas-pie">
+          <canvas ref="bubbleCanvasRef"></canvas>
         </div>
       </div>
-  </template>
+    </div>
+  </div>
+  <div class="card m-4">
+    <div class="card-header">Number of Transactions per Month</div>
+    <div class="card-body d-flex justify-content-center">
+      <div class="canvas">
+        <canvas ref="canvasRef"></canvas>
+      </div>
+    </div>
+  </div>
+</template>
   
 
-  <style scoped>
-  canvas {
-    width: 100%;
-    height: 100%;
-  }
+<style scoped>
+canvas {
+  width: 100%;
+  height: 100%;
+}
 
-  .canvas-pie {
-    width: 100%;
-    max-width: 300px;
-    height: auto;
-  }
+.canvas-pie {
+  width: 100%;
+  max-width: 300px;
+  height: auto;
+}
 
-  .card-header {
-    font-size: 1.15rem;
-  }
+.card-header {
+  font-size: 1.15rem;
+}
 
-  .graph-container {
-    display: flex;
-    flex-wrap: nowrap;
-  }
+.graph-container {
+  display: flex;
+  flex-wrap: nowrap;
+}
 
-  .card {
-    flex: 1;
-    margin-right: 10px;
-  }
+.card {
+  flex: 1;
+  margin-right: 10px;
+}
 
-  .card:first-child canvas {
-    height: 300px;
-  }
+.card:first-child canvas {
+  height: 300px;
+}
 
-  .card:last-child {
-    margin-right: 0;
-  }
+.card:last-child {
+  margin-right: 0;
+}
 
-  /* Updated style for the last card's canvas */
-  .card:last-child canvas {
-    height: 400px; /* Set the desired height */
-  }
+/* Updated style for the last card's canvas */
+.card:last-child canvas {
+  height: 400px;
+  /* Set the desired height */
+}
 </style>
 
