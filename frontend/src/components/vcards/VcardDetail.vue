2bb35a2a340e4a2043192ae9a15bd67a03c9f5ae
Rafael Santos <rafaelfsantos03@gmail.com>
2023-12-15 02:40:22 +0000

    fds

--- frontend/src/components/vcards/VcardDetail.vue
+++ frontend/src/components/vcards/VcardDetail.vue
@@ -6,12 +6,12 @@ import { useUserStore } from "../../stores/users.js";
 
 const router = useRouter();
 const userStore = useUserStore();
-const vcards = ref(null); // Inicialize tasks como null
+const vcards = ref(null);
 
 const loadVCards = async () => {
   try {
     const response = await axios.get("vcard/" + userStore.user.id + "/load");
-    vcards.value = response.data.data; // Atribua response.data.data a tasks.value
+    vcards.value = response.data.data;
     console.log(response.data.data);
   } catch (error) {
     console.log(error);
@@ -25,13 +25,11 @@ defineExpose({ vcards });
 
 <template>
   <div class="card text-center m-4" v-if="vcards">
-    <!-- Verifique se tasks não é null -->
     <div class="card-header bg-dark text-white">
       <h5>{{ vcards.phone_number }}</h5>
     </div>
     <div class="card-body">
       <h5 class="card-title">{{ vcards.name }}</h5>
-      <!-- Acesse as propriedades de tasks -->
       <h3 class="pt-2">BALANCE</h3>
       <h5 class="card-text pb-3 pt-1">{{ vcards.balance }}</h5>
     </div>
