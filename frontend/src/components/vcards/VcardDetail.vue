8a9099c784e75eda97ba2ae7ca92fd5c8513e389
alexmiguel23 <2211044@my.ipleiria.pt>
2023-12-15 20:12:04 +0000

    Grafico a funcionar

--- frontend/src/components/vcards/VcardDetail.vue
+++ frontend/src/components/vcards/VcardDetail.vue
@@ -3,24 +3,77 @@ import { ref, onMounted } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
 import { useUserStore } from "../../stores/users.js";
+import Chart from 'chart.js/auto';
+import { createApp } from 'vue'
+
 
 const router = useRouter();
 const userStore = useUserStore();
 const vcards = ref(null);
+const lineCanvasRef = ref(null);
+const chartData = ref([]);
 
 const loadVCards = async () => {
   try {
     const response = await axios.get("vcard/" + userStore.user.id + "/load");
     vcards.value = response.data.data;
-    console.log(response.data.data);
+    const transactions = response.data.data;
+
   } catch (error) {
     console.log(error);
   }
 };
 
-onMounted(loadVCards);
+const loadTransactions = async () => {
+  try {
+    const response = await axios.get("/transactions");
+    const transactions = response.data.data;
 
-defineExpose({ vcards });
+    // Initialize an array to hold the count of transactions for each month
+    const monthlyTransactionCounts = Array(12).fill(0);
+
+    // Iterate over the transactions
+    for (const transaction of transactions) {
+      // Parse the date of the transaction
+      const date = new Date(transaction.date);
+
+      // Increment the count for the month of the transaction
+      monthlyTransactionCounts[date.getMonth()]++;
+    }
+
+    // Update the chart data
+    chartData.value = monthlyTransactionCounts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Define the function to create the chart
+const createChart = () => {
+  const barCtx = lineCanvasRef.value.getContext('2d');
+
+  new Chart(barCtx, {
+    type: 'bar',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [{
+        label: "Transactions",
+        backgroundColor: "#503596",
+        hoverBackgroundColor: "#503264",
+        borderColor: "black",
+        borderWidth: 2,
+        data: chartData.value,
+      }],
+    },
+  });
+};
+
+// Run the loadVCards function and create the chart when the component is mounted
+onMounted(async () => {
+  await loadTransactions();
+  loadVCards();
+  createChart();
+});
 </script>
 
 <template>
@@ -44,8 +97,6 @@ defineExpose({ vcards });
         </div>
       </div>
     </div>
-
-    <!-- Earnings (Monthly) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-success shadow h-100 py-2">
         <div class="card-body">
@@ -62,8 +113,6 @@ defineExpose({ vcards });
         </div>
       </div>
     </div>
-
-    <!-- Earnings (Monthly) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-info shadow h-100 py-2">
         <div class="card-body">
@@ -82,8 +131,6 @@ defineExpose({ vcards });
         </div>
       </div>
     </div>
-
-    <!-- Pending Requests Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-warning shadow h-100 py-2">
         <div class="card-body">
@@ -101,67 +148,16 @@ defineExpose({ vcards });
       </div>
     </div>
   </div>
-
-  <!-- Content Row -->
   <div class="row">
-
-    <!-- Content Column -->
-    <div class="col-lg-6 mb-4">
-
-      <!-- Project Card Example -->
+    <div class="col-lg-12 mb-4">
       <div class="card shadow mb-4">
         <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
+          <h6 class="m-0 font-weight-bold text-dark">Number of Transactions per Month</h6>
         </div>
         <div class="card-body">
-          <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
-          <div class="progress mb-4">
-            <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0"
-              aria-valuemax="100"></div>
-          </div>
-          <h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>
-          <div class="progress mb-4">
-            <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40"
-              aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-          <h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>
-          <div class="progress mb-4">
-            <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0"
-              aria-valuemax="100"></div>
-          </div>
-          <h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>
-          <div class="progress mb-4">
-            <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0"
-              aria-valuemax="100"></div>
-          </div>
-          <h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>
-          <div class="progress">
-            <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100"
-              aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
+          <canvas ref="lineCanvasRef" width="100" height="25"></canvas>
         </div>
       </div>
-
-    </div>
-
-    <div class="col-lg-6 mb-4">
-
-      <!-- Illustrations -->
-      <div class="card shadow mb-4">
-        <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-        </div>
-        <div class="card-body">
-          <div class="text-center">
-            <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="@/assets/logo.png" alt="...">
-          </div>
-          <p>Irure et dolore veniam mollit. Aliqua qui laboris occaecat excepteur fugiat. Ea nulla eu eu esse labore irure
-            reprehenderit consequat aliqua. Laboris eiusmod minim ipsum ad do velit fugiat consequat voluptate ulla</p>
-        </div>
-      </div>
-
     </div>
   </div>
 </template>
-
-<style></style>
